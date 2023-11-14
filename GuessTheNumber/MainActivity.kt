039949e4826df7780867d7d8be26ba21fa798b97
Екатерина <130843083+KatyaTurusina@users.noreply.github.com>
2023-11-14 23:52:29 +0800

    Update MainActivity.kt

--- GuessTheNumber/MainActivity.kt
+++ GuessTheNumber/MainActivity.kt
@@ -1,1 +1,27 @@
+package com.example.randomNumber
 
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onNextActivity(view: View) {
+        val intent = Intent(applicationContext, GameActivity::class.java)
+        val left_number: EditText = findViewById(R.id.left)
+        val right_number: EditText = findViewById(R.id.right)
+
+        val left: Int = Integer.parseInt(left_number.text.toString())
+        val right: Int = Integer.parseInt(right_number.text.toString())
+
+        intent.putExtra("left", left)
+        intent.putExtra("right", right)
+        startActivity(intent)
+    }
+}
