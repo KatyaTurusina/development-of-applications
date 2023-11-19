d0f2dd31a09fd7175aa04a82776d22425a6da459
Екатерина <130843083+KatyaTurusina@users.noreply.github.com>
2023-11-19 17:52:20 +0800

    Add files via upload

---
+++ RandomFilmJson/Movie.kt
@@ -0,0 +1,3 @@
+package com.example.randomfilm
+
+data class Movie (val name: String, val genre: String, val year: Int, val rating: Float, val country: String, val main_roles: Array<String>, val producer: String) {}
